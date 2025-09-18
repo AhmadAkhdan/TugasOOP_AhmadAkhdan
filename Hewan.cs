0cafa5aeb86ca8dea3e7cd3d4f82ae6e7f1d2f22
AhmadAkhdan <ahmadakhdan1705@gmail.com>
2025-09-18 10:44:20 +0700

    Add files via upload

---
+++ Hewan.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Hewan
+{
+    private string nama; // data hiding
+
+    public Hewan(string nama) // constructor
+    {
+        this.nama = nama;
+    }
+
+    public string Nama // encapsulation
+    {
+        get { return nama; }
+        set { nama = value; }
+    }
+
+    // Polymorphism (akan dioverride)
+    public virtual void Suara()
+    {
+        Console.WriteLine($"{nama} mengeluarkan suara...");
+    }
+}
