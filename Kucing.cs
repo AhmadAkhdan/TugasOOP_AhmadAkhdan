07ac9043c3e110eb49ea36a0d58927b7999d6fa7
AhmadAkhdan <ahmadakhdan1705@gmail.com>
2025-09-23 23:30:44 +0700

    Add files via upload

---
+++ Kucing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+// Inheritance
+public class Kucing : Hewan
+{
+    public Kucing(string nama) : base(nama) { }
+
+    // Polymorphism - Overriding
+    public override void Suara()
+    {
+        Console.WriteLine($"{Nama} berkata: Meong!");
+    }
+
+    // Polymorphism - Overloading
+    public void Suara(string mood)
+    {
+        Console.WriteLine($"{Nama} berkata dengan {mood}: Meooong!");
+    }
+}
---
+++ Pemilik.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Composition
+public class Pemilik
+{
+    public string Nama { get; set; }
+    private List<Hewan> peliharaan;
+
+    public Pemilik(string nama)
+    {
+        Nama = nama;
+        peliharaan = new List<Hewan>();
+    }
+
+    public void TambahPeliharaan(Hewan h)
+    {
+        peliharaan.Add(h);
+    }
+
+    public void TampilkanPeliharaan()
+    {
+        Console.WriteLine($"{Nama} memiliki hewan:");
+        foreach (var h in peliharaan)
+        {
+            Console.Write("- ");
+            h.Suara();
+        }
+    }
+}
